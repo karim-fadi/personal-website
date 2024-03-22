06acc444b38238dd58cce54bdec307ab4282e50f
Karim Fadi <karimfmohsen2005@gmail.com>
2024-03-22 22:35:14 +0200

    Added Optimizations

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,6 @@
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
-
-
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
